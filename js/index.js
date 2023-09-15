32f88b10f943eeb56999e3788713e43dfa9df8b6
[devshourovpro] <[devshourovpro@gmail.com]>
2023-09-15 15:46:09 +0600

    random color added in all card type divs

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 // random color function 
 
+function getRandomColor(){
+    const letters = '0123456789ABCEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+const divS = document.querySelectorAll('.random-color');
+for(const div of divS){
+    div.addEventListener('mouseover', function(){
+        const randomColor = getRandomColor();
+        div.style.backgroundColor = randomColor;
+    })
+    div.addEventListener('mouseout', function(){
+        div.style.backgroundColor = '#FFF';
+    })
+}
