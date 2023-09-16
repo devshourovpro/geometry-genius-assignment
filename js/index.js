b32b3a1d523cf78db92ecbf0448ab0caabaac6ad
[devshourovpro] <[devshourovpro@gmail.com]>
2023-09-16 22:13:08 +0600

    toggle input field added

--- js/index.js
+++ js/index.js
@@ -18,3 +18,34 @@ for(const div of divS){
         div.style.backgroundColor = '#FFF';
     })
 }
+
+
+
+
+// edit button and drop down button 
+document.getElementById('edit-button').addEventListener('click', function(){
+    const dropDown = document.getElementById('drop-down');
+    dropDown.classList.remove('hidden')
+})
+
+
+// click checkbox and add value 
+
+document.getElementById('check-button').addEventListener('click', function(){
+    const checkButton = document.getElementById('check-button');
+    const userInputOne = document.getElementById('user-input-one');
+    const userInputTwo = document.getElementById('user-input-two');
+    const userValueOne = document.getElementById('user-value-one');
+    const userValueTwo = document.getElementById('user-value-two');
+
+    if(checkButton.Checked === false){
+        userValueOne.innerText = '0';
+        userValueTwo.innerText = '0';
+    }
+    else{
+        userValueOne.innerText = userInputOne.value ;
+        userValueTwo.innerText = userInputTwo.value;
+    }
+})
+
+document.getElementById('calculate-btn').
